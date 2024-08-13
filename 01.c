068109f37f5a89afa3e99809e5a1aa057e3c70a8
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-13 09:07:58 -0300

    Update 01.c

--- 01.c
+++ 01.c
@@ -3,17 +3,7 @@
   	exchange rate. Next, show the amount in euros converted into dollars.
  	Youtube lesson: https://youtu.be/IAvfEo8CkWc
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
- 	
 */
-<<<<<<< HEAD
-=======
-/* 
-   	First of all, we have to include the library which contains the function that
-  	will print the information on the screen. Next, start creating the variables 
-	for the euros, dollar exchange rate (der) and the answer (division between euros
- 	and der), after all, print on the screen the answer 
-*/
->>>>>>> 9c82057df2a8c983701a53ff0353e6aec9c9e388
 
 #include<stdio.h>
 
