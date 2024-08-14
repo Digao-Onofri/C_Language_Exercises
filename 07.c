3ce0c98e62962015ad0d8b9a78c06d0d8920a7b8
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-14 16:33:02 -0300

    Update 07.c

--- 07.c
+++ 07.c
@@ -4,7 +4,7 @@
  	distribution percentage and taxes (incidences on the factory cost). Thinking that the 
  	distribution percentage will be 28% and taxes will be 45%, create a program that reads the 
  	factory cost and shows the final price of the car.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/CSGPcpqJxgs
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
