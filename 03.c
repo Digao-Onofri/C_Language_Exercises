2f7d2e034d6afbb348b0a5ec1018d367586261ea
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-15 20:40:07 -0300

    Update 03.c

--- 03.c
+++ 03.c
@@ -3,7 +3,7 @@
  	Example: The leaflet of a pediatric medicine recommends the following dosage: 5 drops for 	
  	every 2 kg of the child's weight. Create a program that reads the child's weight and shows 
  	the number of drops to be administered.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/iGmJ-1eHnvI?si=oTZGQ2KiODZXpaSj
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
