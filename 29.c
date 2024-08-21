c6f30f70a66f559627d78806b5b51e87f3d0da51
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-21 18:11:31 -0300

    Update 29.c

--- 29.c
+++ 29.c
@@ -3,15 +3,9 @@
  	Example: Write a program that reads a year and checks if it is a leap year. A leap year 	
  	is divisible by 400, or if it is divisible by 4, but not by 100. Then, print as an answer 	
  	whether it is or not
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/6bhvfNhy7Jo
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
-*/
-/*
-
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
 */
 
 #include<stdio.h>
