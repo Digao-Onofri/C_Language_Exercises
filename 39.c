3b6b1bf7d81c6ad37c3ada9764936ae258680974
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-29 13:10:07 -0300

    Update 39.c

--- 39.c
+++ 39.c
@@ -3,7 +3,7 @@
  	Example: Write a program that reads an integer value N and prints an increasing count from 0 
  	to N. If the program reads a negative value, keep asking for an integer until the number is 
  	positive.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/ISI_aaVzlC8
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
