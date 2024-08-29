2207d1708d515dd73f9c17621df099f5d5b7b39c
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-29 13:10:24 -0300

    Update 31.c

--- 31.c
+++ 31.c
@@ -7,12 +7,6 @@
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-/*
-
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
 
 #include<stdio.h>
 //start of the program
