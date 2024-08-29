ed65ba89a15f769d66fc62c7891cf689d60e35f3
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-08-29 13:11:44 -0300

    Update 37.c

--- 37.c
+++ 37.c
@@ -6,13 +6,6 @@
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #include<math.h>
 int main()
