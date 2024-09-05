cb7fc275cd603d8a14ad1e18fb0e3c566e713d30
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-05 14:25:01 -0300

    Update 43.c

--- 43.c
+++ 43.c
@@ -1,18 +1,10 @@
 /*
 
  	Example: Write a program that reads two integers and prints the even numbers in that range.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/WF2CRcF1kRY
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 int main()
 {
