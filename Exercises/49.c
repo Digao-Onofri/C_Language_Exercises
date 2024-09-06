658fdee3e2997d76c75b2d4be805cb18bdfa2f45
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-06 18:22:40 -0300

    Update 49.c

--- Exercises/49.c
+++ Exercises/49.c
@@ -2,18 +2,10 @@
 
  	Example:  Make a program that reads an N number and shows the sum of the first N terms of 
  	the following expression: S = 1 – 2 + 3 – 4 + 5 – 6 + ... + N. 
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/ochbA5vEG4M
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 //start of the program
 int main()
