8800b738009ade6915578eda2c3a6c9f7443cfa6
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-07 12:10:47 -0300

    Update 52.c

--- Exercises/52.c
+++ Exercises/52.c
@@ -3,18 +3,10 @@
  	Example:  Make a program that reads an N number and shows the sum of the first N terms of 
  	the following expression, accurate to two decimal places: S = + 1/1 + 3/2 – 5/3 – 7/4 + 9/5 
  	+ 11/6 – 13/7 – 15/8 + ... 
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/bcox8fIz2x0
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 int main()
 {
