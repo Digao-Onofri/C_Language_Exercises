9fafc9dfca91bb8f60beab5ba72841fb721faba6
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-07 12:17:51 -0300

    Update 55.c

--- Exercises/55.c
+++ Exercises/55.c
@@ -3,18 +3,10 @@
  	Example:  Make a program that reads 8 integers and stores them in a vector. Next, read two 
  	integers X and Y. At the end, show the sum of the vector elements that are in the X and Y 
  	positions.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/LSkDeMFZzwA
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #define MAX 8
 int main()
