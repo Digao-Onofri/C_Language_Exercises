3b140212478431ca36da1a65ede7b8d0890bbb5b
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-07 19:03:29 -0300

    Update 58.c

--- Exercises/58.c
+++ Exercises/58.c
@@ -2,18 +2,10 @@
 
  	Example: Write a program, that reads 6 integer values and stores them in a array. Next, 
  	print the reverse of the array.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/NC8NvEvYMXo
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #define MAX 6
 int main()
