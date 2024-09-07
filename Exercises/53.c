d53ee7a6323295933cc5612cf74914e6118d37e2
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-07 18:57:46 -0300

    Update 53.c

--- Exercises/53.c
+++ Exercises/53.c
@@ -1,6 +1,6 @@
 /*
 
- 	Example:  Make a program that reads 6 integer values and stores them in an array. 
+ 	Example:  Make a program that reads 6 integer values and stores them in the array. 
  	Youtube lesson: https://youtu.be/q54nSV9olBA
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
