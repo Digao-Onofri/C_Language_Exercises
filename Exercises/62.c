dbeaffdde52aa7ad3ce88b93037034bd11e4dc8c
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-08 15:29:49 -0300

    Update 62.c

--- Exercises/62.c
+++ Exercises/62.c
@@ -2,16 +2,9 @@
 
  	Example:  Make a program that reads 6 integers and stores them in an array. Then print on a 
  	single line which are the largest and second largest elements present in the array.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/zFZFxZLWopY
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
-*/
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
 */
 #include<stdio.h>
 #define MAX 6
