45bd4dbd46206b2422944f8977752e96e57dbc66
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-09 09:03:18 -0300

    Update 76.c

--- Exercises/76.c
+++ Exercises/76.c
@@ -1,8 +1,8 @@
 /*
 
- 	Example:  Make a program that reads an A matrix of integers (3x3). Next, read a matrix B of 
- 	integers (3x3). Create a matrix C (3x3) composed of the largest values of each position of A 
- 	and B. Print C as answer. 
+ 	Example:  Make a program that reads an A matrix of integers (3x3). Next, read a matrix B (3x3). 
+  	Create a matrix C (3x3) composed of the largest values of each position of A and B. 
+   	Print C as answer. 
  	Youtube lesson: 
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
@@ -17,51 +17,42 @@
 
 #include<stdio.h>
 #define MAX 3
-//start of the program
 int main()
 {
 	int a[MAX][MAX], b[MAX][MAX], c[MAX][MAX], i, j, control;
-	
-	for(i = 0; i < MAX; i++)
-	{
-		for(j = 0; j < MAX; j++)
-		{
+
+	printf("Enter the values of the matrix a:\n");
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){
 			scanf("%d", &a[i][j]);
 		}
 	} 
 
-	for(i = 0; i < MAX; i++)
-	{
-		for(j = 0; j < MAX; j++)
-		{
+	printf("Enter the values of the matrix b:\n");
+	
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){
 			scanf("%d", &b[i][j]);
 		}
 	} 
 
-	for(i = 0; i < MAX; i++)
-	{
-		for(j = 0; j < MAX; j++)
-		{
-			if(a[i][j] > b[i][j])
-			{
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){
+			if(a[i][j] > b[i][j]){
 				c[i][j] = a[i][j];
 			}
-			else
-			{
+			else{
 				c[i][j] = b[i][j];
 			}
 		}
 	}
 
    	control = 0;
-	for(i = 0; i < MAX; i++)
-	{
-		for(j = 0; j < MAX; j++)
-		{      
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){      
                 	printf("%d ", c[i][j]);
                 	control++;
-		        if(control % 3 == 0)
-		        {
+		        if(control % 3 == 0){ // Just to print as a matrix on the terminal
 		            printf("\n");
 		        }
 				
