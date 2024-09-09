6b0b7603d3b78aa92288fc9908ae14a4f29e0721
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-09 08:35:37 -0300

    Update 73.c

--- Exercises/73.c
+++ Exercises/73.c
@@ -1,7 +1,7 @@
 /*
 
  	Example:  Make a program that reads a 4x4 matrix of integers. Next, show as an answer how 
- 	many values greater than 10 are present in the matrix. 
+ 	many values bigger than 10 are present in the matrix. 
  	Youtube lesson: 
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
@@ -16,20 +16,14 @@
 
 #include<stdio.h>
 #define MAX 4
-//start of the program
 int main()
 {
-	int n[MAX][MAX], i, j, control;
+	int matrix[MAX][MAX], i, j, control = 0;
 	
-	control = 0;
-	
-	for(i = 0; i < MAX; i++)
-	{
-		for(j = 0; j < MAX; j++)
-		{
-			scanf("%d", &n[i][j]);
-			if(n[i][j] >= 10)
-			{
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){
+			scanf("%d", &matrix[i][j]);
+			if(matrix[i][j] > 10){
 				control++;
 			}
 		}
