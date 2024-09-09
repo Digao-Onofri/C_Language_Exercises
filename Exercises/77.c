e746ddb2ee7108f8f1fbeacf537552905bc74b5c
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-09 09:16:08 -0300

    Update 77.c

--- Exercises/77.c
+++ Exercises/77.c
@@ -16,39 +16,29 @@
 */
 
 #include<stdio.h>
-//start of the program
 int main()
 {
 	int a[2][4], b[4][3], i, j, k, l;
 	
-	for(i = 0; i < 2; i++)
-	{
-		for(j = 0; j < 4; j++)
-		{
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 4; j++){
 			scanf("%d", &a[i][j]);
 		}
 	}
 	
 	printf("-------");
 
-	for(k = 0; k < 4; k++)
-	{
-		for(l = 0; l < 3; l++)
-		{
+	for(k = 0; k < 4; k++){
+		for(l = 0; l < 3; l++){
 			scanf("%d", &b[k][l]);
 		}
 	}
 
-	for(i = 0; i < 2; i++)
-	{
-		for(j = 0; j < 4; j++)
-		{
-			for(k = 0; k < 4; k++)
-			{
-				for(l = 0; l < 3; l++)
-				{
-					if(a[i][j] == b[k][l])
-					{
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 4; j++){
+			for(k = 0; k < 4; k++){
+				for(l = 0; l < 3; l++){
+					if(a[i][j] == b[k][l]){
 						printf("%d ", a[i][j]);
 					}
 				}
