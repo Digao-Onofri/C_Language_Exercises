d097c1b796f5fdc184d706368d400e77eb940f02
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-09 09:39:10 -0300

    Update 78.c

--- Exercises/78.c
+++ Exercises/78.c
@@ -1,8 +1,7 @@
 /*
 
- 	Example: Faça um programa que leia uma matriz A de inteiros (3x3). A seguir, leia uma matriz 
- 	B de inteiros (4x5).Imprima, em uma linha, separados por um único espaço, os elementos de A 
- 	que não estão presentes em B. 
+ 	Example: Make a program that reads a matrix A of integers (3x3). Next, read a matrix B for integers (4x5).
+  	Print, in a line, separated by single space, the elements of A that are not present in B. 
  	Youtube lesson: 
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
@@ -13,54 +12,42 @@
 	Link for the script is on the video description!
 	Subscribe for regular content!
 
-*/=
+*/
 
 #include<stdio.h>
-//start of the program
 int main()
 {
 	int a[3][3], b[4][5], i, j, k, l, control;
-	
-	for(i = 0; i < 3; i++)
-	{
-		for(j = 0; j < 3; j++)
-		{
+
+	printf("Enter the values of the matrix a:\n");
+	for(i = 0; i < 3; i++){
+		for(j = 0; j < 3; j++){
 			scanf("%d", &a[i][j]);
 		}
 	}
 	
-	printf("-------\n");
-
-	for(k = 0; k < 4; k++)
-	{
-		for(l = 0; l < 5; l++)
-		{
+	printf("Enter the values of the matrix b:\n");
+	for(k = 0; k < 4; k++){
+		for(l = 0; l < 5; l++){
 			scanf("%d", &b[k][l]);
 		}
 	}
 
-	for(i = 0; i < 3; i++)
-	{
+	printf("The values that appears only in matrix A:\n");
+	for(i = 0; i < 3; i++){
 		control = 0;
-		for(j = 0; j < 3; j++)
-		{
-			for(k = 0; k < 4; k++)
-			{
-				for(l = 0; l < 5; l++)
-				{
-					if(a[i][j] == b[k][l])
-					{
+		for(j = 0; j < 3; j++){
+			for(k = 0; k < 4; k++){
+				for(l = 0; l < 5; l++){
+					if(a[i][j] == b[k][l]){
 						control = 1;
-					}
-					
+					}	
 				}
-				if(control == 0)
-				{
+				if(control == 0){
 					printf("%d ", a[i][j]);
 					break;
 				}
 			}
-			
 		}
 	}
 
