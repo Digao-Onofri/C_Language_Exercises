c123f3f051539aa1d02ea562d92bb7ca5109bc40
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-09 16:44:35 -0300

    Update 70.c

--- Exercises/70.c
+++ Exercises/70.c
@@ -1,6 +1,6 @@
 /*
 
- 	Example:  Make a program that reads 10 real values and stores them in a array v. Next, 
+ 	Example:  Make a program that reads 10 real values and stores them in a array . Next, 
  	calculate, with 1 decimal place, the sampling standard deviation of the elements in the 
  	array, according to the following expression, where n is the Quantity of elements sample standard 	 
  	deviation = 1 / N * summatory (v[i] - average)^2 
@@ -38,7 +38,9 @@ int main()
     Sample_Standard_Deviation = sqrt(summatory / MAX); 
     /* If you want to calculate the standard deviation just include (summatory / MAX - 1) in the
     sqrt parenthesis */
-    printf("The sample samples standard deviation is: %.1f", Sample_Standard_Deviation);
+    printf("The sample samples standard deviation is: %.1f\n", Sample_Standard_Deviation);
+     
+    printf("\n");
     
     return 0;
 }
