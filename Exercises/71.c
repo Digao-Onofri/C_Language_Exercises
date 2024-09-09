efa76ba5dcf20535ae2fdcc0687bef1be8327936
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-09 07:33:59 -0300

    Update 71.c

--- Exercises/71.c
+++ Exercises/71.c
@@ -18,35 +18,26 @@
 
 #include<stdio.h>
 #define MAX 10
-//start of the program
 int main()
 {
-	int a[MAX], i, j, k, prime, control;
-
-	i = 0;
+	int a[MAX], i = 0, j, prime, control;
 	
-	while(i < MAX)
-	{
-        	k = 1;
+	while(i < MAX){
+        	control = 1;
         	scanf("%d", &prime);
-        	for(j = 2; j < prime; j++)
-        	{
-        		control = prime % j; 
-        		if(control == 0)
-        	        {
-        			k = 0;
+        	for(j = 2; j < prime; j++){ 
+        		if(prime % j == 0){
+        			control = 0;
         	        	break;
            		}
         	}
-       		if(k == 1 && prime != 1)
-        	{
+       		if(control == 1 && prime != 1){
        			a[i] = prime;
             		i++; 
         	}
     	}
 	
-	for(i = 0; i < MAX; i++)
-	{
+	for(i = 0; i < MAX; i++){
 	    printf("%d ", a[i]);
 	}
 	
