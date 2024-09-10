86abf56a11f7b4d10b955895a0098e6e5ef0088d
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-10 10:28:17 -0300

    Update 89.c

--- Exercises/89.c
+++ Exercises/89.c
@@ -18,23 +18,29 @@
 int main()
 {
 	char a[100], b[100];
-	int answer = 0, tama, tamb;
+	int answer = 0, lenght_a, lenght_b;
+
+	printf("Enter the sentence 1:\n");
+	scanf("%[^\n]", a);
+	printf("Enter the sentence 2:\n");
+	scanf(" %[^\n]", b);
+
+	// We use -1 to reduce the least place that have the \0
+	lenght_a = strlen(a) - 1;
+	printf("The lenght of sentence 1 is: %d\n", lenght_a);
+	lenght_b = strlen(b) - 1;
+	printf("The lenght of sentence 2 is: %d\n", lenght_b);  
 	
-	fgets(a, 100, stdin);
-	fgets(b, 100, stdin);
-	
-	printf("%lu", strlen(a));
-	printf("%lu", strlen(b));
-	
-	if(strlen(a) > strlen(b))
-	{
-		answer = (strlen(a) - 1) - (strlen(b) - 1);
-		printf("A frase 1 possui %d caracteres a mais que a frase 2.", answer);
+	if(lenght_a > lenght_b){
+		answer = lenght_a - lenght_b;
+		printf("Sentence 1 has %d characters more than sentence 2\n", answer);
 	}
-	else if(strlen(a) < strlen(b))
-	{
-		answer = (strlen(b) - 1) - (strlen(a) - 1); 
-		printf("A frase 1 possui %d caracteres a menos que a frase 2.", answer);
+	else if(lenght_a < lenght_b){
+		answer = lenght_b - lenght_a; 
+		printf("Sentence 2 has %d characters more than sentence 1\n", answer);
+	}
+	else if(lenght_a == lenght_b) {
+	   printf("The sentences has the same amount of characters\n");
 	}
 
 	return 0;
