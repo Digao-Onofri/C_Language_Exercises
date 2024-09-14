d0c4b12fd51264bbed991b49ff77368f747fa108
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-14 17:25:25 -0300

    Update 72.c

--- Exercises [01 - 100]/72.c
+++ Exercises [01 - 100]/72.c
@@ -4,7 +4,7 @@
  	a perfect square number, store it in a 10 position array. So the perfect tenth square 
  	number is stored, end the reading and print, in a single line, the array elements separated 
  	from each other for a single space. 
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/ZfmOFGnODIA
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
