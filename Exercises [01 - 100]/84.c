80419793880849fa322fcaf7360feff7d676a6d6
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-28 12:49:03 -0300

    Update 84.c

--- Exercises [01 - 100]/84.c
+++ Exercises [01 - 100]/84.c
@@ -2,18 +2,10 @@
 
  	Example:Make a program that reads a sentence and counts how many vowels the sentence has in 
  	total.
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/1vs2jG6GA78
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #include<string.h>
 int main()
