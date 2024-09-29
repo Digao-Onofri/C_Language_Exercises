2482d4a7bf27ea5dff7022e5e6dc9eb5497bb8b0
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-29 11:23:38 -0300

    Update 88.c

--- Exercises [01 - 100]/88.c
+++ Exercises [01 - 100]/88.c
@@ -3,18 +3,10 @@
  	Example:  Make a program that reads a sentence and check if the sentence is a palindrome, 
  	that is, the same when read from left to right and from right to left. Disregard the spaces 
  	in the sentence during the check. Your program should only print "yes" or "no" as an answer. 
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/_Tor1JiSvuQ
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
