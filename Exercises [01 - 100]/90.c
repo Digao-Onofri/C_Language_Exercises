6e47d94f1e5aa6b2921169701ed0d021ee8e4871
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-09-29 11:31:30 -0300

    Update 90.c

--- Exercises [01 - 100]/90.c
+++ Exercises [01 - 100]/90.c
@@ -2,18 +2,10 @@
 
  	Example:  Make a program that reads one sentence and two integers I and J. Then print the 
  	characters of the positions that go from I to J, inclusive. 
- 	Youtube lesson: 
+ 	Youtube lesson: https://youtu.be/7QTmvUHxjCo
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #include<string.h>
 int main()
