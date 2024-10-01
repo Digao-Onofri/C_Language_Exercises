c56585969bc98080d6d5c0375952aef67f01301b
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-10-01 19:22:37 -0300

    Update 104.c

--- Exercises [101 - 200]/104.c
+++ Exercises [101 - 200]/104.c
@@ -2,7 +2,7 @@
 
  	Example:  Create a structure to store a student's data: name, enrollment number, and course. 
  	Read the information from 5 students and store it in a vector of this structure. Next, print 
- 	the data of all students on the screen. Follow the output format shown in Moodle. 
+ 	the data of all students on the screen. 
  	Youtube lesson: 
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
@@ -18,25 +18,28 @@
 #include<stdio.h>
 #define N 5
 
-typedef struct aluno{
-	char nome[100];
+typedef struct student{
+	char name[100];
 	char enrol_num[100];
-	char curso[100];
-} aluno;
+	char course[100];
+} student;
 
 int main()
 {
-	aluno a[N];
+	student a[N];
 	int i;
 	
 	for(i = 0; i < N; i++){
-		scanf(" %[^\n]", a[i].nome);
+		printf("Enter your name:\n");
+		scanf(" %[^\n]", a[i].name);
+		printf("Enter your enrollment number:\n");
 		scanf(" %s", a[i].enrol_num);
-		scanf(" %[^\n]", a[i].curso);
+		printf("Enter your course:\n");
+		scanf(" %[^\n]", a[i].course);
 	}
 	
 	for(i = 0; i < N; i++){
-		printf("Aluno %d:\nNome: %s\nMatricula: %s\nCurso: %s\n\n", i + 1, a[i].nome, a[i].enrol_num, a[i].curso);
+		printf("Student %d:\nName: %s\nEnrollment number: %s\nCourse: %s\n\n", i + 1, a[i].name, a[i].enrol_num, a[i].course);
 	}
 
 	return 0;
