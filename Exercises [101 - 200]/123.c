3a24dc648559f3999657fe4a7257ca937609c544
Digao <165202041+Digao-paiva@users.noreply.github.com>
2024-12-07 09:31:19 -0300

    updates

--- Exercises [101 - 200]/123.c
+++ Exercises [101 - 200]/123.c
@@ -3,8 +3,7 @@
  	Example:  Make a program that reads 15 lottery numbers (between 1 and 25) bet by the user. 
  	Then, randomly generate the 15 numbers drawn. Next, check how many numbers the user matched 
  	and dynamically create an array of equal size to the number of hits. Using pointer 
- 	arithmetic, store the numbers hit in this array and then show the result. And how many are 
- 	odd. 
+ 	arithmetic, store the numbers hit in this array and then show the result.
  	Youtube lesson: 
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
@@ -22,7 +21,7 @@
 #include<time.h>
 
 int main() {	
-	int i = 0, j = 0, adress = 0, check = 0, flag = 0, qtd = 0, even = 0;
+	int i = 0, j = 0, adress = 0, check = 0, flag = 0;
 	int *bet, *p;
 	int sort[25] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
 	
@@ -43,7 +42,7 @@ int main() {
 	the remainder of the division by 25 */
 	adress = rand() % 25; 
 	//set the value placed in the adress generated 
-	*(p + j) = s[adress]; 
+	*(p + j) = sort[adress]; 
 	printf("Numbers generated:\n");
 	printf("%d ", *(p + j));
 	check++;
@@ -64,17 +63,6 @@ int main() {
 	}
 	
 	printf("\n");
-	/*calculating the quantity of values 
-	in the bet that had randomly generated */
-	for(i = 0; i < 15; i++){
-		for(j = 0; j < 15; j++)
-			if(*(p + j) == *(bet + i)){
-				qtd++;
-				if(*(p + j) % 2 == 0){
-					even++;
-				}
-			}
-	}
 	
 	//printing the values that the guy guessed correctly
 	printf("Numbers guessed correctly:\n");
@@ -84,8 +72,6 @@ int main() {
 				printf("%d ", *(bet + i));
 			}
 	}
-
-	printf("\nAmount of even numbers sorted: %d\n", even);
 	
 	//free the memory that had used in the arrays
 	free(bet);
