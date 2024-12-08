097a37e83457adf6d9552863408ee8bebf733a91
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-12-08 15:45:21 -0300

    Update 123.c

--- Exercises [101 - 200]/123.c
+++ Exercises [101 - 200]/123.c
@@ -8,14 +8,6 @@
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
