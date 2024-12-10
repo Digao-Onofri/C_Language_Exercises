8ea2a245d71236b1fd825871b7c5864898add583
Rodrigo Onofri <165202041+Digao-Onofri@users.noreply.github.com>
2024-12-10 18:19:44 -0300

    Update 126.c

--- Exercises [101 - 200]/126.c
+++ Exercises [101 - 200]/126.c
@@ -7,14 +7,6 @@
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
 */
-
-/*
-	
-	Link for the script is on the video description!
-	Subscribe for regular content!
-
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 
