271505c0fd4c6b49fb3d8d95a6a0f79b681bc8c6
Digao <165202041+Digao-paiva@users.noreply.github.com>
2024-12-13 16:52:19 -0300

    updates

--- Exercises [101 - 200]/144.c
+++ Exercises [101 - 200]/144.c
@@ -1,7 +1,7 @@
 /*
 
- 	Example:   Make a program that receives a number, then using recursion calculate
-    	the fibonacci of the term
+ 	Example: Make a program that receives a number, then using recursion calculate
+    	the fibonacci of the number
  	Youtube lesson: 
  	Author : Rodrigo Onofri @ www.youtube.com/@RodrigoOnofri
  	
@@ -13,17 +13,9 @@
 
 */
 #include<stdio.h>
-#include<stdlib.h>
 
-int fibonacci(int n) {
-    if(n == 0) {
-        return 0;
-    } else if(n == 1) {
-        return 1;
-    } else {
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-}
+int fibonacci(int n);
+
 int main (){
     int n, i;
     printf("Enter the number of terms: \n");
@@ -35,4 +27,16 @@ int main (){
 
     printf("\n");
     return 0;
+}
+
+int fibonacci(int n) {
+    if(n == 0) {
+        return 0; 
+    } 
+    else if(n == 1) {
+        return 1;
+    } 
+    else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
